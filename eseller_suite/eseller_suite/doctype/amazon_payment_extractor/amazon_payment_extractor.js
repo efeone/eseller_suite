61a692385fc6f305f7ee63868488f50029822356
SherinKR <sherinkrply@gmail.com>
2024-05-30 09:47:52 +0530

    feat: Amazon Payment Extractor

--- eseller_suite/eseller_suite/doctype/amazon_payment_extractor/amazon_payment_extractor.js
+++ eseller_suite/eseller_suite/doctype/amazon_payment_extractor/amazon_payment_extractor.js
@@ -3,64 +3,107 @@
 
 frappe.ui.form.on("Amazon Payment Extractor", {
 	refresh: function(frm) {
-    if (!frm.doc.sales_invoice_created) {
-      frm.add_custom_button('Create Sales Invoice', () => {
-        frappe.call({
-          method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.create_sales_invoices',
-          args: {
-            docname: frm.doc.name
-          },
-          freeze: true,
-          callback: (r) => {
-            frm.reload_doc();
-          }
-        });
-      });
-    }
+		if (!frm.doc.sales_invoice_created) {
+			frm.add_custom_button('Sales Invoices', () => {
+				if(frm.doc.missing_items){
+					frm.scroll_to_field('missing_items');
+			        frappe.dom.unfreeze();
+			        frappe.throw({message:__("Please map Missing Items with Product Details."), title: __("Item Mapping Missing")});
+				}
+				else {
+					create_sales_invoices(frm);
+				}
+			}, 'Create');
+		}
 		else if(!frm.doc.amazon_easy_shipping_charge){
 			frm.add_custom_button('Amazon Easy Shipping Charge', () => {
-				frappe.call({
-          method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.update_sales_invoices',
-          args: {
-            docname: frm.doc.name
-          },
-          freeze: true,
-          callback: (r) => {
-            frappe.msgprint(r.message);
-            frm.reload_doc();
-          }
-        });
-      });
-    }
+				add_amazon_shipping_charges(frm)
+			}, 'Add');
+		}
 		else if (!frm.doc.refund){
-			frm.add_custom_button('Refund', () => {
-				frappe.call({
-          method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.update_sales_invoices_refund',
-          args: {
-            docname: frm.doc.name
-          },
-          freeze: true,
-          callback: (r) => {
-            frappe.msgprint(r.message);
-            frm.reload_doc();
-          }
-        });
-      });
-    }
+			frm.add_custom_button('Refund/Return Invoice', () => {
+				create_return_invoice(frm);
+			}, 'Create');
+		}
 		else if (!frm.doc.fulfillment_fee_refund){
 			frm.add_custom_button('Fulfillment Fee Refund', () => {
-				frappe.call({
-          method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.update_sales_invoices_fulfillment_fee_refund',
-          args: {
-            docname: frm.doc.name
-          },
-          freeze: true,
-          callback: (r) => {
-            frappe.msgprint(r.message);
-            frm.reload_doc();
-          }
-        });
-      });
-    }
+				add_fulfillment_fee_refund(frm);
+			}, 'Add');
+		}
+		else if (!frm.doc.invoices_submitted){
+			frm.add_custom_button('Submit Invoices', () => {
+				submit_all_invoices(frm);
+			});
+		}
 	}
 });
+
+function create_sales_invoices(frm){
+	frappe.call({
+		method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.create_sales_invoices',
+		args: {
+			docname: frm.doc.name
+		},
+		freeze: true,
+		freeze_message: __("Creating Sales Invoices..."),
+		callback: (r) => {
+			frm.reload_doc();
+		}
+	});
+}
+
+function add_amazon_shipping_charges(frm){
+	frappe.call({
+		method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.update_sales_invoices_with_shipping_charges',
+		args: {
+			docname: frm.doc.name
+		},
+		freeze: true,
+		freeze_message: __("Adding Amazon Easy Shipping Charges..."),
+		callback: (r) => {
+			frm.reload_doc();
+		}
+	});
+}
+
+function create_return_invoice(frm){
+	frappe.call({
+		method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.create_return_invoice',
+		args: {
+			docname: frm.doc.name
+		},
+		freeze: true,
+		freeze_message: __("Creating Return Invoices..."),
+		callback: (r) => {
+			frm.reload_doc();
+		}
+	});
+}
+
+function add_fulfillment_fee_refund(frm){
+	frappe.call({
+		method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.update_sales_invoices_with_fulfillment_fee_refund',
+		args: {
+			docname: frm.doc.name
+		},
+		freeze: true,
+		freeze_message: __("Adding Fulfillment Fee Refund..."),
+		callback: (r) => {
+			frm.reload_doc();
+		}
+	});
+}
+
+function submit_all_invoices(frm){
+	frappe.call({
+		method: 'eseller_suite.eseller_suite.doctype.amazon_payment_extractor.amazon_payment_extractor.submit_all_invoices',
+		args: {
+			docname: frm.doc.name
+		},
+		freeze: true,
+		freeze_message: __("Submiting Invoices..."),
+		callback: (r) => {
+			frm.reload_doc();
+		}
+	});
+}
