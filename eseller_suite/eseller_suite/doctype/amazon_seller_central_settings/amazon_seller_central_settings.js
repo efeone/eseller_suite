83d44f58207b19d5bb1963e83c7c8359e8664515
Geethu K <geethuk626@gmail.com>
2024-06-28 16:11:37 +0530

    feat:Amazon Api

--- eseller_suite/eseller_suite/doctype/amazon_seller_central_settings/amazon_seller_central_settings.js
+++ eseller_suite/eseller_suite/doctype/amazon_seller_central_settings/amazon_seller_central_settings.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Amazon Seller Central Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Amazon Seller Central Settings", {
+	refresh(frm) {
+		frm.add_custom_button(__('Get Orders'), function(){
+			frappe.call({
+				method: 'eseller_suite.eseller_suite.doctype.amazon_seller_central_settings.amazon_seller_central_settings.get_orders',
+				args: {
+						docname: frm.doc.name
+				},
+				freeze: true,
+				callback: (r) => {
+						frm.reload_doc();
+				}
+			});
+		});
+	},
+});
